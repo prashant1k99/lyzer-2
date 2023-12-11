2ec51b5773af3ba3d1891c719bf3a5e63694dbdf
Prashant <prashantco111@gmail.com>
2023-12-11 11:51:34 +0530

    Add GitHub button to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import ThemeToggler from '@/components/ThemeToggler'
+import Button from './components/ui/button'
+import { BsGithub } from 'solid-icons/bs'
 
 function App() {
 	return (
@@ -7,8 +9,16 @@ function App() {
 				{/* Hello World
 				 */}
 				<div class="h-full w-full rounded-lg p-2">Logo</div>
-				<div class="h-full w-full rounded-lg p-2 flex justify-end">
+				<div class="h-full w-full rounded-lg p-2 flex justify-end items-center gap-4">
 					<ThemeToggler />
+					<a
+						tabIndex={-1}
+						href="https://github.com/prashant1k99/lyzer"
+						target="_blank">
+						<Button size={'icon'} variant={'secondary'}>
+							<BsGithub size={24} />
+						</Button>
+					</a>
 				</div>
 				<div class="border-2 border-gray-500 h-full w-full rounded-lg p-2">
 					Sidebar{' '}
